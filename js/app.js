7ef94229ff5fc0a810818008216216e46150fa29
Maksim Proselkin <proselkinmaksim@gmail.com>
2024-04-21 21:43:46 +0300

    adaptive + nav-menu

--- js/app.js
+++ js/app.js
@@ -9,13 +9,38 @@
 // }
 
 // changeBgImg();
+document.addEventListener('DOMContentLoaded', () => {
+    const menuButton = document.getElementById('menu');
+    const fullscreenMenu = document.getElementById('fullscreen-menu');
+    const closeButton = document.getElementById('close-menu'); // Получаем кнопку закрытия
+  
+    menuButton.addEventListener('click', (event) => {
+      event.stopPropagation(); // Предотвращаем всплытие события, чтобы клик на кнопку не закрывал меню
+      fullscreenMenu.classList.toggle('show');
+    });
+  
+    closeButton.addEventListener('click', () => { // Добавляем обработчик событий для кнопки закрытия
+      fullscreenMenu.classList.remove('show');
+    });
+  
+    document.addEventListener('click', (event) => {
+      const isClickInsideMenu = fullscreenMenu.contains(event.target);
+      const isClickOnMenuButton = (event.target === menuButton);
+  
+      if (!isClickInsideMenu && !isClickOnMenuButton && fullscreenMenu.classList.contains('show')) {
+        fullscreenMenu.classList.remove('show');
+      }
+    });
+  
+    document.addEventListener('keydown', (event) => {
+      if (event.key === 'Escape' && fullscreenMenu.classList.contains('show')) {
+        fullscreenMenu.classList.remove('show');
+      }
+    });
+  });
+  
 
-
-$(document).bind( 'mousewheel', function (e) { 
-    var nt = $(document.body).scrollTop()-(e.deltaY*e.deltaFactor*100); 
-    e.preventDefault(); 
-    e.stopPropagation(); 
-    $(document.body).stop().animate( { 
-         scrollTop : nt 
-     } , 500 , 'easeInOutCubic' );  
-} )
+  
+  
+  
+  
